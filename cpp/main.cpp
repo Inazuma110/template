fb4baa41f3504552c35ba114179561cb79e38c53
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-05-17 13:31:18 +0900

    fix: cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,12 @@
+#include <bits/stdc++.h>
+
 using namespace std;
 
-#include <iostream>
-#include <cstdlib>
-#include <algorithm>
-#include <cmath>
-#include <limits>
-#include <climits>
-#include <vector>
-#include <string>
-#include <set>
-#include <queue>
-#include <stack>
-#include <map>
-#include <numeric>
 typedef long long ll;
 typedef pair<int,int> p;
 
-int main()
+
+int main(void)
 {
 
   return 0;

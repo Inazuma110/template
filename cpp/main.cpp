ada6fbc7124881e93c8cc6838eb5547ae904950f
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-06-17 00:54:47 +0900

    add: rall()

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define itn int
 typedef long long ll;
 typedef pair<int,int> p;

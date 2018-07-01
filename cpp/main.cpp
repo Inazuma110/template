24436fed241c3f5167668a5c0afd2e7ec6bbf0cb
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-07-02 02:47:22 +0900

    define int

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define itn int
+#define itn long long
+
 typedef long long ll;
 typedef pair<ll,ll> p;
 
 
 int main()
 {
+  #define int long long
 
 }

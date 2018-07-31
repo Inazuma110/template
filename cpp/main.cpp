fe745fa4034db16187024c803b5825a84dc221eb
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-07-31 19:22:52 +0900

    fix cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define itn long long
+#define itn int
 
 typedef long long ll;
 typedef pair<ll,ll> p;

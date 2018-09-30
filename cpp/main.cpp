8dfb4a9ac8f3aa488e069ea3caaa3562ca6cecdb
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-09-30 17:36:21 +0900

    print関数追加

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,17 +1,77 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define itn int
 
 typedef long long ll;
 typedef pair<ll,ll> p;
 
 
-int main()
-{
+// prototype declare to print
+struct Container;
 
+template <class>
+struct Ignore;
+
+template <class, class X>
+struct PrintObj;
+
+template<class T>
+struct PrintObj<T, typename Ignore<typename T::iterator>::type>;
+
+template<class T>
+void print(T value);
+
+template<class T, class U>
+ostream& operator << (ostream& os, const pair<T, U> p);
+
+//////////////////////////////////////////////
+
+
+int main(void){
+
+}
+
+
+
+template <class>
+struct Ignore{
+  typedef Container type;
+};
+
+template <class T, class X=Container>
+struct PrintObj{
+  void operator()(T value){
+    cout << value;
+  }
+};
+
+template<class T>
+struct PrintObj<T, typename Ignore<typename T::iterator>::type>{
+  void operator()(T value){
+    // cout << endl << "[";
+    cout << "[";
+    bool isFirst = true;
+    for(auto a : value){
+      if(!isFirst){
+        cout << ", ";
+      }
+      PrintObj<typename T::value_type>()(a);
+      isFirst = false;
+    }
+    cout << "]";
+  }
+};
+
+template<class T>
+void print(T value){
+  PrintObj<T>()(value);
+  cout << endl;
+}
+
+template<class T, class U>
+ostream& operator << (ostream& os, const pair<T, U> p){
+  os << "(" << p.first << " : " << p.second << ")";
+  return os;
 }

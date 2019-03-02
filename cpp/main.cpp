d40127fcc01b2929aeb7f1932b061f69e85d9e17
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-03-03 01:44:15 +0900

    add mod

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,10 +7,13 @@ using namespace std;
 
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
+#define MOD 1000000007
+#define INF 100100100
 
 typedef long long ll;
 typedef pair<int, int> p;
 
-int main(){
 
+int main(){
+  cout << MOD << endl;
 }

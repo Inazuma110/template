321d8508749e9616437c189492304221b80c74a3
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-07-30 16:08:58 +0900

    add rep

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
   #include "print.hpp"
 #endif
 
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define ALL(x) (x).begin(), (x).end()
 #define RALL(x) (x).rbegin(), (x).rend()
 #define MOD 1000000007
